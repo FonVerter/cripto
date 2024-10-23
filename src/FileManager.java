364b906254542c8dad91712bb0bfc37abfd42ec6
alexander <drommefangerr@gmail.com>
2024-10-23 20:28:25 +0300

    finished reading

---
+++ src/FileManager.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileManager {
+
+    private String originalFilePath;
+    private String pathToTheModifiedFile; //нужен ли он здесь?
+    private Path path;
+    Files files;
+
+    public FileManager(String originalFilePath) {
+        this.originalFilePath = originalFilePath;
+    }
+
+    public String getOriginalFilePath() {
+        return originalFilePath;
+    }
+
+    public void setOriginalFilePath(String originalFilePath) {
+        this.originalFilePath = originalFilePath;
+    }
+
+    public byte[] readTheUserFile (){
+        path = Path.of(originalFilePath);
+        path = path.toAbsolutePath();
+        byte [] originalFileBytesArray = null;
+        try {
+            originalFileBytesArray = Files.readAllBytes(path);
+            System.out.println("Успешно прочел файл" + "\nКоличество байт в файле: " + originalFileBytesArray.length);
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
+        return originalFileBytesArray;
+    }
+
+    public byte[] writeTheUserFile(byte [] modifiedFileBytesArray){
+
+        return null;
+    }
+
+
+}
